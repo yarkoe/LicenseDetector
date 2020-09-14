2ed8e1f32c5b26136c6167d28703eaa1a4e48ee6
Koekin Yaroslav <ykoekin@gmail.com>
2020-09-14 09:27:53 +0300

    added tests

---
+++ src/test/LicenseDetectorTest.kt
@@ -0,0 +1,33 @@
+package test
+
+import LicenseDetector
+import LicenseInfo
+import LicenseType
+import NullLicenseInfo
+import junit.framework.Assert.*
+import org.junit.Test
+import java.nio.file.Paths
+
+class LicenseDetectorTest {
+
+    @Test
+    fun testLicenseDetectorSimple() {
+        val licenseDetector = LicenseDetector(Paths.get("src/").toAbsolutePath().toString() + "/test/TestProject")
+
+        val actualMainLicenseType = LicenseType.LGPL_3_0
+        val actualLicenseTypes = listOf(
+                LicenseType.GPL_3_0,
+                LicenseType.Apache_2_0,
+                LicenseType.LGPL_3_0
+        )
+
+        val testResults = licenseDetector.detect()
+        val testLicenseTypes = testResults.licensesInfo.map{ it.licenseType }
+
+        assertFalse(testResults.mainLicenseInfo is NullLicenseInfo)
+        assertEquals((testResults.mainLicenseInfo as LicenseInfo).licenseType, actualMainLicenseType)
+
+        assertEquals(testResults.licensesInfo.count(), 3)
+        assertEquals(testLicenseTypes, actualLicenseTypes)
+    }
+}
